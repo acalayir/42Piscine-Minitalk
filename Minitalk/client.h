23651c0c5f73632bd4aac5c196ca1fb65fd95907
acalayir <101639800+acalayir@users.noreply.github.com>
2022-03-17 15:21:56 +0300

    Add files via upload

---
+++ Minitalk/client.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalayir <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 16:28:09 by acalayir          #+#    #+#             */
+/*   Updated: 2022/02/16 16:28:13 by acalayir         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(char *str);
+void	ft_putnbr(int nb);
+int		ft_atoi(char *str);
+
+#endif
---
+++ Minitalk/client_utils.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalayir <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 16:28:22 by acalayir          #+#    #+#             */
+/*   Updated: 2022/02/16 16:28:27 by acalayir         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client.h"
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	return (count);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+	{
+		c = nb + 48;
+		write (1, &c, 1);
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (str[i] == ' ' || str[i] == '\n')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res);
+}
